9f43d8dded5045bb87b43c5fc01771b74c67e8f2
Jip Verveda <jibril.bear@gmail.com>
2023-02-10 19:47:53 +0100

    Added GXPEngine

---
+++ GXPEngine/MyGame.cs
@@ -0,0 +1,17 @@
+using System;
+using GXPEngine;
+using System.Drawing;
+
+public class MyGame : Game {
+	public MyGame() : base(800, 600, false) {
+
+	}
+
+	void Update() {
+		
+	}
+
+	static void Main() {
+		new MyGame().Start();
+	}
+}
